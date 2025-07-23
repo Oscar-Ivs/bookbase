d844b19b3c4391f5891f1fea85f00cf01b31f9a9
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-07-23 15:21:56 +0100

    Installed python Pillow

--- books/models.py
+++ books/models.py
@@ -18,3 +18,18 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+    
+    from django.contrib.auth.models import User
+    from django.db import models
+
+    from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
